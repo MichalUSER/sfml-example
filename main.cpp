89ee5a6d46d7097d6bbad9c56d542edf497027bd
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2020-07-15 08:59:14 +0200

    initial

--- main.cpp
+++ main.cpp
@@ -5,13 +5,30 @@ using namespace std;
 
 int main()
 {
+	float shapeWidth;
+	float shapeHeight;
 	float xpos = 390.f;
 	float ypos = 100.f;
 	float speed = 9.f;
+	float spaceAround = 15.f;
 	sf::RenderWindow window(sf::VideoMode(900, 500), "SFML Example");
 	window.setFramerateLimit(60.f);
 
-	sf::RectangleShape shape(sf::Vector2f(120.f, 50.f));
+	sf::Font font;
+	if(!font.loadFromFile("roboto.ttf"))
+	{
+		cout << "Cannot open font\n";
+	}
+	sf::Text text;
+	text.setFont(font);
+	text.setString("Click Me!");
+	text.setCharacterSize(24);
+	text.setFillColor(sf::Color::White);
+	shapeWidth = text.getLocalBounds().width + spaceAround * 2;
+	shapeHeight = text.getLocalBounds().height + spaceAround * 2;
+	text.setPosition(sf::Vector2f(xpos + spaceAround, ypos + spaceAround));
+
+	sf::RectangleShape shape(sf::Vector2f(shapeWidth, shapeHeight));
 	shape.setFillColor(sf::Color(238, 82, 83));
 	shape.setPosition(sf::Vector2f(xpos, ypos));
 
@@ -30,30 +47,34 @@ int main()
 		{
 			ypos += speed;
 			shape.setPosition(sf::Vector2f(xpos, ypos));
+	text.setPosition(sf::Vector2f(xpos + spaceAround, ypos + spaceAround));
 		}
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
 			ypos -= speed;
 			shape.setPosition(sf::Vector2f(xpos, ypos));
+			text.setPosition(sf::Vector2f(xpos + spaceAround, ypos + spaceAround));
 		}
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
 			xpos -= speed;
 			shape.setPosition(sf::Vector2f(xpos, ypos));
+			text.setPosition(sf::Vector2f(xpos + spaceAround, ypos + spaceAround));
 		}
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
 			xpos += speed;
 			shape.setPosition(sf::Vector2f(xpos, ypos));
+			text.setPosition(sf::Vector2f(xpos + spaceAround, ypos + spaceAround));
 		}
 
 		shape.setFillColor(sf::Color(238, 82, 83));
 		if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
 			sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-			if(mousePosition.x > xpos && mousePosition.x < xpos + 120)
+			if(mousePosition.x > xpos && mousePosition.x < xpos + shapeWidth)
 			{
-				if(mousePosition.y > ypos && mousePosition.y < ypos + 50)
+				if(mousePosition.y > ypos && mousePosition.y < ypos + shapeHeight)
 				{
 					shape.setFillColor(sf::Color(255, 107, 107));
 				}
@@ -62,6 +83,7 @@ int main()
 
 		window.clear();
 		window.draw(shape);
+		window.draw(text);
 		window.display();
 	}
 
